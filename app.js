fe9f48d9b8e1fe5c887d7f8d1a3f3f001ed300d3
KhangBM <nguyenquockhang120901@gmail.com>
2024-01-11 16:43:13 +0700

    Game Dev

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop()){
+            $('nav').addClass('scroll')
+        }
+        else{
+            $('nav').removeClass('scroll')
+        }
+    });
+});
